f19babfabb734611403f8f36874d661bdf1b91a4
yuanjie.liu <Kevinliuxian@outlook.com>
2021-10-08 00:11:01 +0800

    add service collection extensions

---
+++ JOIEnergy/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JOIEnergy.Domain.Interfaces;
+using JOIEnergy.Infrastructure.EF;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JOIEnergy.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IUnitOfWork, JOIEnergyDbContext>();
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+            return services;
+        }
+    }
+}
