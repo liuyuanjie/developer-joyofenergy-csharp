77fd0549e2907c29e2d251d4f8688c4595dee6d4
yuanjie.liu <Kevinliuxian@outlook.com>
2021-10-08 00:10:00 +0800

    add error handler middleware

---
+++ JOIEnergy/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using JOIEnergy.Application.Exception;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace JOIEnergy.Middlewares
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (error)
+                {
+                    case ApiException e:
+                        // custom application error
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+
+                    case KeyNotFoundException e:
+                        // not found error
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+
+                    default:
+                        // unhandled error
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+                var result = JsonSerializer.Serialize(error.Message);
+
+                await response.WriteAsync(result);
+            }
+        }
+    }
+
+    public static class ErrorHandlerExtension
+    {
+        public static void UseErrorHandler(this IApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.UseMiddleware<ErrorHandlerMiddleware>();
+        }
+    }
+}
