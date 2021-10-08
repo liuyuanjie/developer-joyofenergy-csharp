f7c5bb8bb8210727f4de9c787038c4918dcbcb9f
yuanjie.liu <Kevinliuxian@outlook.com>
2021-10-08 08:28:14 +0800

    fix the output json datetime format to utc.

--- JOIEnergy.Application/Commands/StoreReadingCommand.cs
+++ JOIEnergy.Application/Commands/StoreReadingCommand.cs
@@ -26,27 +26,14 @@ namespace JOIEnergy.Application.Commands
 
         public async Task<int> Handle(ElectricityReadingCommand request, CancellationToken cancellationToken)
         {
-            if (!IsMeterReadingsValid(request))
-            {
-                throw new ApiException("Can not be empty.");
-            }
-
             var smartMeter = _repository.Query().FirstOrDefault(x => x.SmartMeterId == request.SmartMeterId)
                              ?? SmartMeter.Create(request.SmartMeterId);
 
             request.ElectricityReadingModels
-                .ForEach(electricityReading => smartMeter.CreateElectricityReading(electricityReading.Time, electricityReading.Reading));
+                .ForEach(electricityReading => smartMeter.CreateElectricityReading(electricityReading.Time.ToUniversalTime(), electricityReading.Reading));
 
             _repository.Update(smartMeter);
             return await _repository.UnitOfWork.CommitAsync();
-        }
-
-        private bool IsMeterReadingsValid(ElectricityReadingCommand electricityReadingCommand)
-        {
-            var smartMeterId = electricityReadingCommand.SmartMeterId;
-            List<ElectricityReadingModel> electricityReadings = electricityReadingCommand.ElectricityReadingModels;
-            return smartMeterId != null && smartMeterId.Any()
-                                        && electricityReadings != null && electricityReadings.Any();
         }
     }
 }
--- JOIEnergy.Infrastructure.Share/DateTimeConverter.cs
+++ JOIEnergy.Infrastructure.Share/DateTimeConverter.cs
@@ -7,6 +7,6 @@ namespace JOIEnergy.Infrastructure.Share
     public class DateTimeConverter : JsonConverter<DateTime>
     {
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => DateTime.Parse(reader.GetString());
-        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString("yyyy-MM-ddTHH:mm:ss.FFFFFFFzzz"));
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToUniversalTime().ToString("O"));
     }
 }
