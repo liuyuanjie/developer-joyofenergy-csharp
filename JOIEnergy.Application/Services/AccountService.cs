18ab268ee219584751a99017a9ef618a5917452b
yuanjie.liu <Kevinliuxian@outlook.com>
2021-10-14 11:49:39 +0800

    improve the sql query.

--- JOIEnergy.Application/Services/AccountService.cs
+++ JOIEnergy.Application/Services/AccountService.cs
@@ -14,11 +14,11 @@ namespace JOIEnergy.Application.Services
 
         public AccountEnergyCompanyPricePlanModel GetAccountEnergyCompanyPricePlanForSmartMeterId(string smartMeterId)
         {
-            var sql = "SELECT e.[Name] AS [CompanyName], e.[Supplier] AS [CompanySupplier], p.[Name] AS [PricePlanName], a.[Name] AS [AccountName] FROM [EnergyCompanies] AS e " +
-                      "INNER JOIN [PricePlans] AS p ON e.[Id] = p.[EnergyCompanyId] " +
-                      "INNER JOIN [SmartMeters] AS s ON p.[Id] = s.[EnergyCompanyPricePlanId] " +
-                      "INNER JOIN [Accounts] AS a ON a.[Id] = s.[AccountId] " +
-                      "WHERE s.[SmartMeterId] = @smartMeterId";
+            var sql = @"SELECT e.[Name] AS [CompanyName], e.[Supplier] AS [CompanySupplier], p.[Name] AS [PricePlanName], a.[Name] AS [AccountName] FROM [EnergyCompanies] AS e 
+                      INNER JOIN [PricePlans] AS p ON e.[Id] = p.[EnergyCompanyId]
+                      INNER JOIN [SmartMeters] AS s ON p.[Id] = s.[EnergyCompanyPricePlanId]
+                      INNER JOIN [Accounts] AS a ON a.[Id] = s.[AccountId]
+                      WHERE s.[SmartMeterId] = @smartMeterId";
             using (var conn = _connection.OpenConnection())
             {
                 var result = conn.ExecuteScalar<AccountEnergyCompanyPricePlanModel>(sql, new { smartMeterId });
